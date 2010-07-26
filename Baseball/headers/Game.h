9dc8014ce6ca47da7edd05ced54f3b3990cc2135
nonsensicalthinking <nonsensicalthinking@56862dcf-c2e8-eb98-9294-4fccf4203350>
2010-07-26 06:59:23 +0000

    Had a thought, decided to encapsulate the keyboard console routines in the Game class.  Now when creating a game you don't need to handle console input and actions, its handled by its parent Game class.

--- Baseball/headers/Game.h
+++ Baseball/headers/Game.h
@@ -5,10 +5,12 @@
  *      Author: brof
  */
 
+#include "Scene.h"
 #include <iostream>
 
 using namespace std;
 
+extern Scene* getScene();
 
 
 class Game	{
@@ -32,17 +34,95 @@ public:
 	}
 
 	// Event handlers
-	virtual void keyPressedEvent(unsigned char key, int x, int y)	{
+	void keyPressedEvent(unsigned char key, int x, int y)	{
+		Scene* curScene = getScene();
+
+		if( curScene->con->consoleActive )	{	// send key input to console
+			switch(key)	{
+				case CONSOLE_KEY:	// deactivate console
+					curScene->con->consoleActive = !curScene->con->consoleActive;
+					break;
+				case ESC_KEY:
+					curScene->con->clearInput();
+					break;
+				default:	// add to input line
+					curScene->con->appendToInput(key);
+			}
+		}
+		else	{	// Don't send key input to console
+			keyPressed(key, x, y);
+		}
+	}
+
+	void specialKeyPressedEvent(int key, int x, int y)	{
+		Scene* curScene = getScene();
+
+		if( curScene->con->consoleActive )	{	// send key input to console
+			switch(key)	{
+				case F1_KEY:
+					break;
+				case F2_KEY:
+					break;
+				case F3_KEY:
+					break;
+				case F4_KEY:
+					break;
+				case F5_KEY:
+					break;
+				case F6_KEY:
+					break;
+				case F7_KEY:
+					break;
+				case F8_KEY:
+					break;
+				case F9_KEY:
+					break;
+				case F10_KEY:
+					break;
+				case F11_KEY:
+					break;
+				case F12_KEY:
+					break;
+				case ARROW_UP_KEY:
+					break;
+				case ARROW_DOWN_KEY:
+					break;
+				case PAGE_UP_KEY:
+					curScene->con->scrollUp();
+					break;
+				case PAGE_DOWN_KEY:
+					curScene->con->scrollDown();
+					break;
+				case HOME_KEY:
+					break;
+				case END_KEY:
+					break;
+				case INSERT_KEY:
+					break;
+				case NUM_LOCK_KEY:
+					break;
+
+			}
+		}
+		else	{
+			specialKeyPressed(key, x, y);
+		}
+	}
+
+	// This is to be overridden
+	virtual void keyPressed(unsigned char key, int x, int y)	{
 
 	}
 
-	virtual void specialKeyPressedEvent(int key, int x, int y)	{
+	// This is to be overridden
+	virtual void specialKeyPressed(int key, int x, int y)	{
 
 	}
+
+	// This is to be overridden
 	virtual void mouseEvent(int button, int state, int x, int y)	{
 
 	}
-
 
 
 };
--- Baseball/headers/GameTest.h
+++ Baseball/headers/GameTest.h
@@ -9,7 +9,6 @@
 
 #define CAM_MOVE_RATE 20
 
-extern Scene* getScene();
 
 
 // inherit the game class
@@ -55,108 +54,45 @@ public:
 
 	// Event handlers
 	// Handles keyboard input from normal text keys
-	virtual void keyPressedEvent(unsigned char key, int x, int y)	{
+	virtual void keyPressed(unsigned char key, int x, int y)	{
 		Scene* curScene = getScene();
-
-		if( curScene->con->consoleActive )	{	// send key input to console
-			switch(key)	{
-				case CONSOLE_KEY:	// deactivate console
-					curScene->con->consoleActive = !curScene->con->consoleActive;
-					break;
-				case ESC_KEY:
-					curScene->con->clearInput();
-					break;
-				default:	// add to input line
-					curScene->con->appendToInput(key);
-			}
-		}
-		else	{	// Don't send key input to console
-			switch(key)	{
-				case '`':	// active console
-					curScene->con->consoleActive = !curScene->con->consoleActive;
-					break;
-				case 'a':	// omg our first control over the scene!
-					curScene->cam->moveCameraLeft(CAM_MOVE_RATE);
-					break;
-				case 'd':	// omg our first control over the scene!
-					curScene->cam->moveCameraRight(CAM_MOVE_RATE);
-					break;
-				case 'w':	// omg our first control over the scene!
-					curScene->cam->moveCameraForward(CAM_MOVE_RATE);
-					break;
-				case 's':	// omg our first control over the scene!
-					curScene->cam->moveCameraBack(CAM_MOVE_RATE);
-					break;
-				case 'q':	// omg our first control over the scene!
-					curScene->cam->moveCameraUp(CAM_MOVE_RATE);
-					break;
-				case 'e':	// omg our first control over the scene!
-					curScene->cam->moveCameraDown(CAM_MOVE_RATE);
-					break;
-				case 'z':
-					curScene->doItAgain();
-					break;
-				case ESC_KEY:
-					curScene->exit();
-					break;
-			}
+		cout << "Key pressed" << endl;
+		switch(key)	{
+			case '`':	// active console
+				curScene->con->consoleActive = !curScene->con->consoleActive;
+				break;
+			case 'a':	// omg our first control over the scene!
+				curScene->cam->moveCameraLeft(CAM_MOVE_RATE);
+				break;
+			case 'd':	// omg our first control over the scene!
+				curScene->cam->moveCameraRight(CAM_MOVE_RATE);
+				break;
+			case 'w':	// omg our first control over the scene!
+				curScene->cam->moveCameraForward(CAM_MOVE_RATE);
+				break;
+			case 's':	// omg our first control over the scene!
+				curScene->cam->moveCameraBack(CAM_MOVE_RATE);
+				break;
+			case 'q':	// omg our first control over the scene!
+				curScene->cam->moveCameraUp(CAM_MOVE_RATE);
+				break;
+			case 'e':	// omg our first control over the scene!
+				curScene->cam->moveCameraDown(CAM_MOVE_RATE);
+				break;
+			case 'z':
+				curScene->doItAgain();
+				break;
+			case ESC_KEY:
+				curScene->exit();
+				break;
 		}
 	}
 
 
 
 	// handles keyboard input from special keys
-	virtual void specialKeyPressedEvent(int key, int x, int y)	{
-	//	cout << "Special Key Pressed: " << key << endl;
-		Scene* curScene = getScene();
-
-		if( curScene->con->consoleActive )	{	// send key input to console
-			switch(key)	{
-				case F1_KEY:
-					break;
-				case F2_KEY:
-					break;
-				case F3_KEY:
-					break;
-				case F4_KEY:
-					break;
-				case F5_KEY:
-					break;
-				case F6_KEY:
-					break;
-				case F7_KEY:
-					break;
-				case F8_KEY:
-					break;
-				case F9_KEY:
-					break;
-				case F10_KEY:
-					break;
-				case F11_KEY:
-					break;
-				case F12_KEY:
-					break;
-				case ARROW_UP_KEY:
-					break;
-				case ARROW_DOWN_KEY:
-					break;
-				case PAGE_UP_KEY:
-					curScene->con->scrollUp();
-					break;
-				case PAGE_DOWN_KEY:
-					curScene->con->scrollDown();
-					break;
-				case HOME_KEY:
-					break;
-				case END_KEY:
-					break;
-				case INSERT_KEY:
-					break;
-				case NUM_LOCK_KEY:
-					break;
-
-			}
-		}
+	virtual void specialKeyPressed(int key, int x, int y)	{
+		cout << "Special Key Pressed: " << key << endl;
 	}
 
 	virtual void mouseEvent(int button, int state, int x, int y)	{
